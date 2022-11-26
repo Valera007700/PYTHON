b18324ee40cc3aad2297c9d56bca86aa0fc37739
valery <valeradoroshkov987@gmail.com>
2022-11-26 17:02:20 +0100

    26.11

---
+++ Ex_1.py
@@ -0,0 +1,21 @@
+# x = input(str("x = "))
+# y = input(str("y = "))
+# word = input("word = ")
+
+# if 45<49:
+#     print(True)
+# else: print(False)
+# name = ['Vano', 'Petya', 'Gavril', 'Andrew']
+
+
+# i = 1;
+# for kol in ['Vano', 'Petya', 'Gavril', 'Andrew']:
+#     print("#", i, kol)
+#     i +=1
+
+gpa = int(input("Твоё GPA "))
+mlm = str(input("Bread or water:"))
+if  gpa >= 12 and  mlm == "Bread":
+    print("you are right")
+else:
+    print("get out of here")
